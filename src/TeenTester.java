9fdaaf7238b2e246bc8022faad0d36fde7470639
foxrw1 <78601276+foxrw1@users.noreply.github.com>
2021-10-18 13:28:12 -0400

    Update TeenTester.java

--- src/TeenTester.java
+++ src/TeenTester.java
@@ -4,7 +4,7 @@ public class TeenTester
     {
         // Create a new Teen object and print it out; see the Teen class file
         // to see how the constructor and toString method work.
-        Teen myFriend = new Teen("Sonequa", "Martin-Green", 10, true);
+        Teen myFriend = new Teen("Molly", "Fox", 11, true);
         System.out.println(myFriend.toString());
         
         // Ask the user to input a text message
